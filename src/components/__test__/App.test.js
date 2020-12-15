4ecd7694ef705f96797e4fbd5931840399afee49
Cory Morrissey <corymorrissey@lithia.com>
2020-12-14 16:24:03 -0800

    section 16

---
+++ src/components/__test__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+/**
+ * App Test
+ * @function it > Global Function, able to be called without an import
+ * @param {string} > Describes what the test does
+ * @param {function} > Block of code to execute your test
+ */
+
+it('shows a comment box', () => {});
