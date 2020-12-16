3e068f685298598b62f96405b918c3f2d87d10ca
Cory Morrissey <corymorrissey@lithia.com>
2020-12-15 16:57:26 -0800

    section 31/32/33

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-beforeEach(() => {});
+let wrapper;
 
-it('has a text area and a button', () => {});
+beforeEach(() => {
+	wrapper = mount(<CommentBox />);
+});
+
+afterEach(() => {
+	// Unmount the component from the the DOM that JSDOM creates for us
+	wrapper.unmount();
+});
+
+it('has a text area and a button', () => {
+	// Good way to make sure you are on the right track is to console log some of the methods we might want to use in our assertion
+	// console.log(wrapper.find('textarea').length);
+	// console.log(wrapper.find('button').length);
+
+	expect(wrapper.find('textarea').length).toEqual(1);
+	expect(wrapper.find('button').length).toEqual(1);
+});
