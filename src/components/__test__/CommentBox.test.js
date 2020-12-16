5f6aec8424dfb3abc98e3f55d6f892771a454a38
Cory Morrissey <corymorrissey@lithia.com>
2020-12-15 16:44:07 -0800

    section 30

---
+++ src/components/__test__/CommentBox.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+beforeEach(() => {});
+
+it('has a text area and a button', () => {});
