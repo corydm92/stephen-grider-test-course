7b1c6fb2e2c3ed05817508d663df8bc95492e943
Cory Morrissey <corymorrissey@lithia.com>
2020-12-15 16:40:09 -0800

    section 29

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function CommentBox() {
+	const [state, setState] = useState({ comment: '' });
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// TODO - Call an action creator
+		// Save comment
+
+		setState({ comment: '' });
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<h4>Add a Comment</h4>
-			<textarea />
+			<textarea
+				value={state.comment}
+				onChange={(e) => setState({ ...state, comment: e.target.value })}
+			/>
 			<div>
 				<button>Submit Comment</button>
 			</div>
