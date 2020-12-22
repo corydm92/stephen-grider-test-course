a39296c5c0376112306fcaed281282264224b449
Cory Morrissey <corymorrissey@lithia.com>
2020-12-21 16:55:12 -0800

    section 50

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { store } from 'store';
 
 export default function CommentList() {
-	return <div>CommentList</div>;
+	const { state } = useContext(store);
+
+	const renderComments = () => {
+		return state.comments.map((comment, index) => (
+			<li key={index}>{comment}</li>
+		));
+	};
+
+	return (
+		<div>
+			<ul>{renderComments()}</ul>
+		</div>
+	);
 }
