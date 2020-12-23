62323fd4de2a76fac6af5079a40c6b7b494d9d0e
Cory Morrissey <corymorrissey@lithia.com>
2020-12-22 16:07:19 -0800

    section 53

--- src/components/__test__/CommentList.test.js
+++ src/components/__test__/CommentList.test.js
@@ -16,4 +16,19 @@ beforeEach(() => {
 	);
 });
 
-it('creates one LI per comment', () => {});
+it('creates one LI per comment', () => {
+	expect(wrapper.find('li').length).toEqual(2);
+});
+
+it('has correct comment text', () => {
+	// Enzyme documentation says to stay away from the text() method, and to use render() instead
+	// expect(wrapper.text()).toContain('Comment 1');
+
+	// Cherio is a library similar to jQuery
+	// render() Documentation - Returns a CheerioWrapper around the rendered HTML of the single node's subtree.
+	// It must be a single-node wrapper.
+
+	// You must do two tests for this, because you cannot run two toContain() methods in a single assertion.
+	expect(wrapper.render().text()).toContain('Comment 1');
+	expect(wrapper.render().text()).toContain('Comment 2');
+});
