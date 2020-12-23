5464bac7741cd67283143c30eba029aa3e10a5c2
Cory Morrissey <corymorrissey@lithia.com>
2020-12-23 12:33:03 -0800

    middleware example

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,70 @@
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import axios from 'axios';
+
+// https://stackoverflow.com/questions/59438480/react-context-custom-middleware-instead-of-redux
+export const applyApiMiddleware = (dispatch) => (action) => {
+	switch (action.type) {
+		case 'TEST':
+			console.log('test');
+			dispatch({
+				type: 'TEST',
+				payload: {
+					data: 'foo',
+				},
+			});
+			break;
+		case 'REQUEST_GET_USERS':
+			fetch('/users', {
+				method: 'GET',
+			})
+				.then((res) =>
+					dispatch({
+						type: 'REQUEST_GET_USERS_SUCCESS',
+						payload: res.data,
+					})
+				)
+				.catch((error) => console.warn(error));
+			break;
+		case FETCH_COMMENTS:
+			axios
+				.get('https://jsonplaceholder.typicode.com/comments')
+				.then((response) => {
+					// console.log(comments);
+					const comments = response.data
+						.slice(0, 5)
+						.map((comment) => comment.name);
+					console.log('in middleware', response);
+					return dispatch({ type: FETCH_COMMENTS, payload: comments });
+				})
+				.catch((error) => console.warn(error));
+			break;
+		default:
+			console.log('DEFAULT');
+			dispatch(action);
+		//....
+	}
+};
 
 export function saveComment(comment) {
 	return { type: SAVE_COMMENT, payload: comment };
 }
+
+// export async function fetchComments(dispatch) {
+// 	const response = await axios.get(
+// 		'https://jsonplaceholder.typicode.com/comments'
+// 	);
+
+// 	console.log(response);
+
+// 	// const comments = response.data.slice(0, 5);
+
+// 	const comments = [1, 2, 3, 4, 5, 6].slice(0, 5);
+
+// 	// console.log(comments);
+// 	return dispatch({ type: FETCH_COMMENTS, payload: comments });
+// }
+
+export const fetchComments = () => {
+	console.log('in action');
+	return { type: FETCH_COMMENTS };
+};
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,10 +1,13 @@
 import { initialState } from 'store';
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 export default function commentsReducer(state = initialState.comments, action) {
+	console.log('inside reducer', action);
 	switch (action.type) {
 		case SAVE_COMMENT:
 			return [...state, action.payload];
+		case FETCH_COMMENTS:
+			return [...state, ...action.payload];
 		default:
 			return state;
 	}
