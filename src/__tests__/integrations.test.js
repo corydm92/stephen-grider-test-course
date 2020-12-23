885efe3084998337846df8cac15d512cb6be2a66
Cory Morrissey <corymorrissey@lithia.com>
2020-12-23 15:45:16 -0800

    section 61:

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -3,7 +3,11 @@ import { mount } from 'enzyme';
 import { StoreProvider } from 'store';
 import App from 'components/App';
 
+import moxios from 'moxios';
+// Its sole purpose is to fake out requests. We can tell this library to watch for an axios request, and if it gets one we will trick axios into thinking it instantly gets a response. No network request will be created, so the instance axios trys to make an http request moxios is going to axe that and say "nope, youre not allowed to make network requests", and instead of denying it or failing the request, moxios will turn around and say "Hey, dont worry about it the request didn't actually get issued but here is some data that you should use in place of the response"
+
 it('can fetch a list of comments and display them', () => {
+	// Attempt to render the entire app
 	const wrapper = mount(
 		<StoreProvider>
 			<App />
@@ -11,12 +15,11 @@ it('can fetch a list of comments and display them', () => {
 	);
 
 	console.log(wrapper.debug());
-
+	// Find the 'fetchComments' button and click it
 	wrapper.find('[data-test="fetch-button"]').simulate('click');
 
+	// Expect to find a list of comments
 	expect(wrapper.find('li').length).toEqual(5);
 
-	// Attempt to render the entire app
-	// Find the 'fetchComments' button and click it
-	// Expect to find a list of comments
+	// Trying to simulate an api call inside the console will cause the test to break. That is because the JSDOM API is a 'fake' browser we are runing our components in, so when we try to make an axios request to the outside world it fails because we are working in a fake environment. We dont have the ability to make ajax requests from inside our test suite when we are making use of JSDOM.
 });
