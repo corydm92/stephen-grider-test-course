79e67218ab5fb032af58572751ad0f9751b0f556
Cory Morrissey <corymorrissey@lithia.com>
2020-12-23 12:35:08 -0800

    adding async example in main

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,54 @@
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import axios from 'axios';
+
+// https://stackoverflow.com/questions/59438480/react-context-custom-middleware-instead-of-redux
+export const applyApiMiddleware = (dispatch) => (action) => {
+	switch (action.type) {
+		case 'TEST':
+			console.log('test');
+			dispatch({
+				type: 'TEST',
+				payload: {
+					data: 'foo',
+				},
+			});
+			break;
+		case 'REQUEST_GET_USERS':
+			fetch('/users', {
+				method: 'GET',
+			})
+				.then((res) =>
+					dispatch({
+						type: 'REQUEST_GET_USERS_SUCCESS',
+						payload: res.data,
+					})
+				)
+				.catch((error) => console.warn(error));
+			break;
+		case FETCH_COMMENTS:
+			break;
+		default:
+			console.log('DEFAULT');
+			dispatch(action);
+		//....
+	}
+};
 
 export function saveComment(comment) {
 	return { type: SAVE_COMMENT, payload: comment };
+}
+
+export async function fetchComments(dispatch) {
+	const response = await axios.get(
+		'https://jsonplaceholder.typicode.com/comments'
+	);
+
+	console.log(response);
+
+	// const comments = response.data.slice(0, 5);
+
+	const comments = [1, 2, 3, 4, 5, 6].slice(0, 5);
+
+	// console.log(comments);
+	return dispatch({ type: FETCH_COMMENTS, payload: comments });
 }
