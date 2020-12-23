8e0530086e448127964b2800176669fcd351c95f
Cory Morrissey <corymorrissey@lithia.com>
2020-12-23 15:27:33 -0800

    section 58

---
+++ src/__tests__/commentsIntegrations.test.js
@@ -0,0 +1,5 @@
+// This file would be used if our app grew to the point of having many integration tests. This file would be where all comment integration tests would be housed.
+
+it('commentsIntegration placeholder', () => {
+	expect(1).toEqual(1);
+});
---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { StoreProvider } from 'store';
+import App from 'components/App';
+
+it('can fetch a list of comments and display them', () => {
+	// Attempt to render the entire app
+	// Find the 'fetchComments' button and click it
+	// Expect to find a list of comments
+});
