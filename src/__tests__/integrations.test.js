8b95ee47156925db1093b7cbbc6fdf0ea6c8fcb7
Cory Morrissey <corymorrissey@lithia.com>
2020-12-23 15:29:11 -0800

    section 59

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -4,6 +4,12 @@ import { StoreProvider } from 'store';
 import App from 'components/App';
 
 it('can fetch a list of comments and display them', () => {
+	const wrapper = mount(
+		<StoreProvider>
+			<App />
+		</StoreProvider>
+	);
+
 	// Attempt to render the entire app
 	// Find the 'fetchComments' button and click it
 	// Expect to find a list of comments
